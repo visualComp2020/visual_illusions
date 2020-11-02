0ce5ef78923b86c776a1b92d6e8648158b791a19
nfmorenog <nfmorenog@unal.edu.co>
2020-11-01 22:01:00 -0500

    penrose
    penrose triangle using three js

---
+++ Penrose/js/main.js
@@ -0,0 +1,109 @@
+
+import * as THREE from "/build/three.module.js";
+import {OrbitControls} from "/jsm/controls/OrbitControls.js";
+
+var container;
+var sceneWidth;
+var sceneHeight;
+var scene;
+var renderer;
+var camera;
+var controls;
+
+init();
+
+function init(){
+    createScene();
+    update();
+}
+
+function createScene(){
+    sceneWidth =  640;
+    sceneHeight = 480;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
+
+
+    renderer = new THREE.WebGLRenderer( {antialias:true, alpha: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(sceneWidth,sceneHeight);
+    
+    //canvas
+    container = document.getElementById("container");  
+    container.appendChild(renderer.domElement);
+    //camara 
+    
+
+    camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000);
+     
+    //camera = new THREE.PerspectiveCamera(60, sceneWidth / sceneHeight,1,2000);
+    camera.position.set(500,500,500);
+    
+    //luz
+
+    let light = new THREE.DirectionalLight(0xffffff, 1.8);
+    light.position.set(60, 100, 20);
+    scene.add(light);
+
+    /*
+    var hemi = new THREE.HemisphereLight(0xffffff,0xffffff,0.5);
+    hemi.position.set(0,0,5);
+    scene.add(hemi);
+*/
+    controls = new OrbitControls (camera ,renderer.domElement );
+    controls.update();
+
+    //creacion objeto y malla
+   /* var cubeGeo = new THREE.BoxGeometry(10,10,10);
+    var cubeMaterial = new THREE.MeshLambertMaterial( {color:0xee1122});
+    var mesh = new THREE.Mesh(cubeGeo,cubeMaterial);
+    scene.add(mesh);
+*/
+
+
+    let material = new THREE.MeshLambertMaterial({color: "lightgreen", flatShading: true})
+
+    let bottomBox = new THREE.BoxGeometry(50,50,300)
+    let bottomMesh = new THREE.Mesh(bottomBox, material)
+    scene.add(bottomMesh)
+
+    let upBox = new THREE.BoxGeometry(50,300,50)
+    let upMesh = new THREE.Mesh(upBox, material)
+    upMesh.position.set(0,125,125)
+    scene.add(upMesh)
+
+    let topBox = new THREE.BoxGeometry(150,50,50)
+    let topMesh = new THREE.Mesh(topBox, material)
+    topMesh.position.set(100,250,125)
+    scene.add(topMesh)
+
+    let topPlane = new THREE.PlaneGeometry(50,50,32)
+    let topPlaneMesh = new THREE.Mesh(topPlane, material)
+    topPlaneMesh.position.set(200,275,125)
+    topPlaneMesh.rotation.x = THREE.Math.degToRad(-90)
+    scene.add(topPlaneMesh)
+
+    let topTriGeometry = new THREE.Geometry()
+    topTriGeometry.vertices.push(new THREE.Vector3(0,0,0))
+    topTriGeometry.vertices.push(new THREE.Vector3(50,0,0))
+    topTriGeometry.vertices.push(new THREE.Vector3(50,50,0))
+    topTriGeometry.faces.push(new THREE.Face3(0,1,2))
+    topTriGeometry.computeFaceNormals()
+
+    let topTriMesh = new THREE.Mesh(topTriGeometry, material)
+    topTriMesh.position.set(225, 275, 150)
+    topTriMesh.rotation.z = THREE.Math.degToRad(180)
+    scene.add(topTriMesh)
+
+}
+
+function update() {
+    requestAnimationFrame( update);
+    render();
+}
+
+function render(){
+    controls.update();
+    renderer.render(scene,camera);
+}
